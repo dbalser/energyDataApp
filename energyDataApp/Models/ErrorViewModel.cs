a3ce29eec0edd4192624570155dbf6901f52eab0
Brodie Balser <balserbrodie@gmail.com>
2018-11-21 11:08:21 -0700

    first commit... Hello World

---
+++ energyDataApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace energyDataApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
